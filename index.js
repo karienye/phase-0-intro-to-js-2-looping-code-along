313ee0743414833691d2766c70924c5cc17e21f0
leah karienye <karienyeleah@gmail.com>
2022-06-04 01:28:08 +0300

    commit tree 866f14199c8101fdd50f22df0e5a194c9e418175

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  } */
+  
+  const cards = (names, "birthday");
+
+function writeCards(names, event) {
+    return names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+console.log(writeCards(names, "birthday"))
+
+const start = 10, stop = 0, skip = 1;
+
+  for  (let i = start; i >= stop; i = skip) {
+     console.log(i);
+  }
+
+function countDown(i){
+    return console.log(i <= 11 )  
+    console.log (countDown(i))
+}
+
+
